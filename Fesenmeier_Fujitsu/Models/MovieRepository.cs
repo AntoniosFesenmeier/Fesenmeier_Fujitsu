c7f876a61757516446b7ea3c2b6d9a5a87b1511c
Toni <Toni@DESKTOP-U80726J>
2019-09-17 10:33:09 +0200

    remove old mock

--- Fesenmeier_Fujitsu/Models/MovieRepository.cs
+++ Fesenmeier_Fujitsu/Models/MovieRepository.cs
@@ -11,53 +11,6 @@ namespace Fesenmeier_Fujitsu.Models
 {
     public class MovieRepository : IMovieRepository
     {
-
-        //private List<Movie> _movieList;
-
-        //public MockMovieRepository()
-        //{
-        //    _movieList = new List<Movie>()
-        //    {
-        //        new Movie(){ Id = "1", Title = "Apocalypse Now", Director = "Francis Ford Coppola", Actors = "Martin Sheen, Marlon Brando, Robert Duvall", Image = "https://m.media-amazon.com/images/M/MV5BZTNkZmU0ZWQtZjQzMy00YTNmLWFmN2MtZGNkMmU1OThmMGYwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,652,1000_AL_.jpg", Year = 1979 },
-        //        new Movie(){ Id = "2", Title = "A clockwork orange", Director = "Stanley Kubrick", Actors = "Malcolm McDowell, Patrick Magee",  Image = "https://m.media-amazon.com/images/M/MV5BMTY3MjM1Mzc4N15BMl5BanBnXkFtZTgwODM0NzAxMDE@._V1_SY1000_CR0,0,675,1000_AL_.jpg", Year = 1971 }
-        //    };
-        //}
-
-        //public Movie Add(Movie movie)
-        //{
-        //    movie.Id = _movieList.Max(m => m.Id) + 1;
-        //    _movieList.Add(movie);
-        //    return movie;
-        //}
-
-        //public void Delete(string Id)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //public Movie Edit(Movie movie)
-        //{
-        //    var oldMovie = _movieList.FirstOrDefault(m => m.Id == movie.Id);            
-        //    if(oldMovie != null)
-        //    {
-        //        var index = _movieList.IndexOf(oldMovie);
-        //        _movieList.RemoveAt(index);
-        //        _movieList.Insert(index, movie);
-        //    }
-
-        //    return movie;   
-        //}
-
-        //public Movie GetMovie(string Id)
-        //{
-        //    return _movieList.FirstOrDefault(x => x.Id == Id);
-        //}
-
-        //public List<Movie> GetMovies()
-        //{
-        //    return _movieList;
-        //}
-
         private readonly IConfiguration _config;
         private readonly MongoDBContext _context = null;
 
